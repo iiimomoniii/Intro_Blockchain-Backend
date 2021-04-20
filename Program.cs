ed23a206a7a3434c0f0c22ca1ec03de1f6aff4ad
iiimomoniii <natdanai_thaiyala@live.com>
2021-04-20 07:07:23 +0100

    create function for check money in wallet

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
 ﻿
 using Nethereum.HdWallet;
+using Nethereum.Web3;
 using System.Diagnostics;
+using System.Threading.Tasks;
+
 namespace EthereumWallet
 {
     class Program
     {
-        static void Main(string[] args)
+        //server 
+        public static readonly string SERVER = "https://rinkeby.infura.io/v3/6be6a5e9cdb644cdb0ad99c77ae815e4";
+        static async Task Main(string[] args)
         {
-          //0. result => cypto ramdom private key for owner of wallet
-          //hurt test spare where age vapor advice elder betray column scan measure police bacon auto dial diesel lesson that opinion memory speed another eye
-
-          //1. use private key for get wallet
           var wallet = new Wallet("hurt test spare where age vapor advice elder betray column scan measure police bacon auto dial diesel lesson that opinion memory speed another eye","");
           var account = wallet.GetAccount(0);
           var mnemonic = string.Join(" ", wallet.Words); 
-          //3. print wallet from private key
           Debug.WriteLine(account.Address);
-          //result
-          //0xFe8dC03EA6E8784C1f7BfB5Da508505609fE85C0
+          //6. call CheckBalance
+          await CheckBalance(account.Address);
+          //7. if function is correct will return result is => Balance : 0
+        }
+        //1. CheckBalance Method for check money in wallet
+        public static async Task CheckBalance(string address){
+            //2. network for Ethereum between client and server
+            //3. require server Ex. infura.io (End point RINKEBY)
+            var client = new Web3(SERVER);
+            //4. check eth balance (return eth in wei)
+            var ethBalance = await client.Eth.GetBalance.SendRequestAsync(address);
+            //5. convert value wei to money
+            var balance = Web3.Convert.FromWei(ethBalance.Value);
+            //Result
+            Debug.WriteLine($"Balance : {balance}");
+            
         }
     }
 }
